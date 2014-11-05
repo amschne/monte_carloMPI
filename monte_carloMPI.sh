5828f6543cbbfc66df801c9fd69bf5b6374ab5d7
amaschne <amschne@umich.edu>
2014-11-05 15:49:57 -0500

    created bash PBS script to submit MPI job

---
+++ monte_carloMPI.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#PBS -S /bin/sh
+#PBS -N your-mpi-job
+#PBS -l procs=12,mem=1gb,walltime=0:01:00
+#PBS -A climate_flux
+#PBS -l qos=flux
+#PBS -q flux
+#PBS -M amschne@umich.edu
+#PBS -m abe
+#PBS -j oe
+#PBS -V
+#
+echo "I ran on:"
+cat $PBS_NODEFILE
+#cd ~/your_stuff
+
+# Use mpirun to run with 12 cores
+mpirun ./your-mpi-program
