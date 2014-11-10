e195a6e3df6b22d8d47664785f8bdea6ac618993
Adam Michael Schneider <amschne@flux-login1.engin.umich.edu>
2014-11-10 13:11:14 -0500

    fixed bash run script to load proper modules and find the correct exec files

--- monte_carloMPI.sh
+++ monte_carloMPI.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #PBS -S /bin/sh
 #PBS -N your-mpi-job
-#PBS -l procs=12,mem=1gb,walltime=0:01:00
+#PBS -l procs=12,pmem=1gb,walltime=0:30:00
 #PBS -A climate_flux
 #PBS -l qos=flux
 #PBS -q flux
@@ -9,10 +9,16 @@
 #PBS -m abe
 #PBS -j oe
 #PBS -V
-#
+
+module unload openmpi
+module load gcc/4.4.6
+module load openmpi/1.6.0/gcc/4.4.6
+module load python
+module load mpi4py/mpi-1.6.0/gcc-4.4.6/1.3
+
 echo "I ran on:"
 cat $PBS_NODEFILE
-#cd ~/your_stuff
+cd ~/monte_carloMPI
 
 # Use mpirun to run with 12 cores
-mpirun mpi4py-examples/01-hello-world
+mpirun mpi4py-examples/08-matrix-matrix-product.py
