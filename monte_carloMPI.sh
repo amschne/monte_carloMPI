1f1b78b14fc7f2f30ff2be5ed0a65b7504cb56a3
Adam Schneider <amschne@umich.edu>
2015-03-27 01:11:01 -0400

    adjusted PBS script to auto set CASENAME

--- monte_carloMPI.sh
+++ monte_carloMPI.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #PBS -S /bin/sh
-#PBS -N your-mpi-job
-#PBS -l procs=12,pmem=1gb,walltime=0:30:00
+##PBS -N your-mpi-job
+#PBS -l procs=12,pmem=1gb,walltime=1:00:00
 #PBS -A climate_flux
 #PBS -l qos=flux
 #PBS -q flux
@@ -18,7 +18,7 @@ module load mpi4py/mpi-1.6.0/gcc-4.4.6/1.3
 
 echo "I ran on:"
 cat $PBS_NODEFILE
-cd ~/monte_carloMPI
+cd $CASEROOT
 
-# Use mpirun to run with 12 cores
-mpirun monte_carlo3D-run.py
+# Use mpirun to run with n cores (#PBS -l procs=...)
+mpirun -N $CASE monte_carlo3D-run.py
