b58763e8ac47cc890df9e5e6329b22e2f72b300e
Adam Schneider <amschne@umich.edu>
2015-03-27 01:16:20 -0400

    fixed model_setup.sh

--- model_setup.sh
+++ model_setup.sh
@@ -11,9 +11,12 @@ mkdir $DATA/$CASE
 # CREATE CASEROOT DIR, COPY RUN SCRIPTS, CONFIG FILE, POST PROCESSING SCRIPT
 mkdir $CASEROOT
 cp $MONTE_CARLO_ROOT/monte_carlo3D-run.py $CASEROOT
-cp $MONTE_CARLO_ROOT/monte_carloMPI.sh $CASEROOT
+cp $MONTE_CARLO_ROOT/monte_carloMPI.pbs $CASEROOT
 cp $MONTE_CARLO_ROOT/config.ini $CASEROOT
-CP $MONTE_CARLO_ROOT/post_processing.py $CASEROOT
+cp $MONTE_CARLO_ROOT/post_processing.py $CASEROOT
+
+# COPY THIS SCRIPT TO CASEROOT FOR FUTURE REFERENCE
+cp $MONTE_CARLO_ROOT/model_setup.sh $CASEROOT
 
 cd $CASEROOT
 
@@ -24,4 +27,4 @@ echo 'CASEROOT (cwd) setup'
 echo 'Edit config.ini for basic configuration options'
 echo 'In config.ini, be sure to set output_dir=$DATA/$CASE'
 echo 'Edit monte_carlo3D-run.py to setup run parameters'
-echo 'Finally, $ qsub monte_carloMPI.sh # to submit job'
+echo 'Finally, $ qsub monte_carloMPI.pbs # to submit job'
