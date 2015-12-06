7d81f1a1b8de120bc52fb53ecebdad421ac3dcb0
Adam Schneider <amschne@umich.edu>
2015-12-06 17:48:22 -0500

    trying to make mass proc work for nrows or ncols ==1

--- monte_carloMPI/mass_processing.py
+++ monte_carloMPI/mass_processing.py
@@ -33,7 +33,9 @@ class Subplots(object):
         ncols = len(shapes)
         fig, axarr = plt.subplots(nrows, ncols,sharex='col', sharey='row',
                                   figsize=(ncols*4, nrows*3))
-        if nrows==1 or ncols==1:
+        if nrows==1 and ncols==1:
+            axarr = np.array([axarr])
+        if nrows==1 or ncols==1:    
             axarr.shape = (nrows, ncols)
         
         fontsize = 12#40./ncols
